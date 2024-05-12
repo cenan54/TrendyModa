fe37c4ac236b51ff40688cad4d79cd0d5b9e9241
cenan54 <54mustafacenan54@gmail.com>
2024-05-12 22:25:57 +0300

    first image upload function
    added upload post function but it doesn't work properly

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,12 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using TrendyModa.Models;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace TrendyModa.Controllers
 {
+    [Authorize]
     public class PostController : Controller
     {
+        private readonly TrendyModaDbContext context;
+        public PostController()
+        {
+            context = new TrendyModaDbContext();
+        }
+
+        [HttpGet]
         public IActionResult PostAdd()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAddAction(IFormFile imageFile, string description)
+        {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await imageFile.CopyToAsync(memoryStream);
+                    byte[] imageBytes = memoryStream.ToArray();
+
+                    // Yeni bir User nesnesi oluşturun ve veritabanına kaydedin.
+                    var photo = new Photo
+                    {
+                        UserId = Convert.ToInt32(User.FindFirst(x => x.Type == "UserId").Value),
+                        Description = description,
+                        Image = imageBytes
+                        // Diğer sütunlar için de benzer şekilde...
+                    };
+                    context.Photos.Add(photo);
+                    await context.SaveChangesAsync();
+                }
+            }
+
+            return RedirectToAction("Index","Home"); // veya başka bir view'a yönlendirme yapın.
+        }
+
+
+
+
+
+
     }
+
 }
+
--- obj/Debug/net8.0/TrendyModa.AssemblyInfo.cs
+++ obj/Debug/net8.0/TrendyModa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrendyModa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abac8acd090b2afe330b4bda3f525b82a399b2f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrendyModa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrendyModa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
